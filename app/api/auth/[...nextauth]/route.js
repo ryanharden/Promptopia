801376e300b73ea6297d5f7dc95ddbe67a09c66c
Ryan Harden <ryanharden20@gmail.com>
2023-07-10 15:57:58 -0700

    nextauth functionality done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,28 +1,48 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@/utils/database";
+import User from "@models/user";
+
 const handler = NextAuth({
-    provider: [
+    providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async sesssion({ session }) {
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
+            session.user.id = sessionUser._id.toString();
 
-            // check if a user already exists
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
 
-            // if not, create a new user
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
 
-            return true;
-        } catch (error) {
-            console.log(error)
-            return false;
+                // if not, create a new user
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false;
+            }
         }
     }
 })
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,24 +6,25 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
+
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     }, [])
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/" className="flex gap-2 flex-center">
             <Image
-            src="/assets/images/logo.svg"
+            src={"/assets/images/logo.svg"}
             alt="Promptopia Logo"
             width={30}
             height={30}
@@ -34,7 +35,7 @@ const Nav = () => {
 
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex gap-3 md:gap-5">
                     <Link href="/create-prompt" className="black_btn">
                         Create Post
@@ -44,12 +45,13 @@ const Nav = () => {
                     </button>
                     <Link href="/profile">
                         <Image
-                        src="/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className="rounded-full"
                         alt="profile"
                         />
+
                     </Link>
                 </div>
             ): (
@@ -66,10 +68,10 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex">
                     <Image
-                        src="/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className="rounded-full"
